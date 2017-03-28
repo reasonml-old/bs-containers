d9cb2f210aebdf064a54292a57e03cf0b3d86754
glennsl <glenn@slotte.net>
2017-03-28 08:12:01 +0200

    [bresult] camelCase and cleanup

--- src/bresult.ml
+++ src/bresult.ml
@@ -1,6 +1,20 @@
 
 (* This file is free software, part of containers. See file "license" for more details. *)
 
+(*
+
+- [x] Use `camelCase` instead of `snake_case`
+- [x] Get rid of scary monadic and math-y terminology
+- [x] Follow the conventions used in the `Js.*` modules
+- [ ] Type functions correctly, e.g. `compare` should return a proper variant, not `0`, `1` or `-1`
+- [x] Remove operators and aliases, e.g. `pure` as an alias for `return`
+- [ ] Remove the use of exceptions for internal logic (maybe? seems very sketchy to me but perhaps there's a really really good reason, see [this example](https://github.com/BuckleTypes/bs-containers/blob/master/src/bopt.ml#L163))
+- [ ] Replace `Format.printf` calls, they pull in a lot stuff for little benefit
+- [ ] Document everything properly, with examples
+- [ ] Add tests for everything
+
+*)
+
 (** {1 Error Monad} *)
 
 type 'a sequence = ('a -> unit) -> unit
@@ -14,28 +28,15 @@ type (+'good, +'bad) t = ('good, 'bad) Result.result =
   | Ok of 'good
   | Error of 'bad
 
-let return x = Ok x
+let make x = Ok x
 
 let fail s = Error s
 
-let fail_printf format =
-  let buf = Buffer.create 64 in
-  Printf.kbprintf
-    (fun buf -> fail (Buffer.contents buf))
-    buf format
-
-let fail_fprintf format =
-  let buf = Buffer.create 64 in
-  let out = Format.formatter_of_buffer buf in
-  Format.kfprintf
-    (fun out -> Format.pp_print_flush out (); fail (Buffer.contents buf))
-    out format
-
-let of_exn e =
+let fromException e =
   let msg = Printexc.to_string e in
   Error msg
 
-let of_exn_trace e =
+let fromExceptionTrace e =
   let res = Printf.sprintf "%s\n%s"
       (Printexc.to_string e) (Printexc.get_backtrace ())
   in
@@ -45,7 +46,7 @@ let map f e = match e with
   | Ok x -> Ok (f x)
   | Error s -> Error s
 
-let map_err f e = match e with
+let mapError f e = match e with
   | Ok _ as res -> res
   | Error y -> Error (f y)
 
@@ -53,21 +54,21 @@ let map2 f g e = match e with
   | Ok x -> Ok (f x)
   | Error s -> Error (g s)
 
-let iter f e = match e with
+let forEach f e = match e with
   | Ok x -> f x
   | Error _ -> ()
 
-exception Get_error
+exception GetError
 
-let get_exn = function
+let getOrRaise = function
   | Ok x -> x
-  | Error _ -> raise Get_error
+  | Error _ -> raise GetError
 
-let get_or e ~default = match e with
+let getOr e ~default = match e with
   | Ok x -> x
   | Error _ -> default
 
-let map_or f e ~default = match e with
+let mapOr f e ~default = match e with
   | Ok x -> f x
   | Error _ -> default
 
@@ -75,13 +76,9 @@ let catch e ~ok ~err = match e with
   | Ok x -> ok x
   | Error y -> err y
 
-let flat_map f e = match e with
+let flatMap f e = match e with
   | Ok x -> f x
   | Error s -> Error s
-
-let (>|=) e f = map f e
-
-let (>>=) e f = flat_map f e
 
 let equal ?(err=Pervasives.(=)) eq a b = match a, b with
   | Ok x, Ok y -> eq x y
@@ -94,15 +91,15 @@ let compare ?(err=Pervasives.compare) cmp a b = match a, b with
   | _, Ok _ -> -1
   | Error s, Error s' -> err s s'
 
-let fold ~ok ~error x = match x with
+let reduce ~ok ~error x = match x with
   | Ok x -> ok x
   | Error s -> error s
 
-let is_ok = function
+let isOk = function
   | Ok _ -> true
   | Error _ -> false
 
-let is_error = function
+let isError = function
   | Ok _ -> false
   | Error _ -> true
 
@@ -112,31 +109,29 @@ let guard f =
   try Ok (f ())
   with e -> Error e
 
-let guard_str f =
+let guardToString f =
   try Ok (f())
-  with e -> of_exn e
+  with e -> fromException e
 
-let guard_str_trace f =
+let guardToStringTrace f =
   try Ok (f())
-  with e -> of_exn_trace e
+  with e -> fromExceptionTrace e
 
 let wrap1 f x =
-  try return (f x)
+  try make (f x)
   with e -> Error e
 
 let wrap2 f x y =
-  try return (f x y)
+  try make (f x y)
   with e -> Error e
 
 let wrap3 f x y z =
-  try return (f x y z)
+  try make (f x y z)
   with e -> Error e
 
 (** {2 Applicative} *)
 
-let pure = return
-
-let (<*>) f x = match f with
+let apply f x = match f with
   | Error s -> fail s
   | Ok f -> map f x
 
@@ -152,7 +147,7 @@ let both x y = match x,y with
 
 (** {2 Collections} *)
 
-let map_l f l =
+let mapList f l =
   let rec map acc l = match l with
     | [] -> Ok (List.rev acc)
     | x::l' ->
@@ -163,7 +158,7 @@ let map_l f l =
 
 exception LocalExit
 
-let fold_seq f acc seq =
+let foldSeq f acc seq =
   let err = ref None in
   try
     let acc = ref acc in
@@ -175,7 +170,7 @@ let fold_seq f acc seq =
   with LocalExit ->
   match !err with None -> assert false | Some s -> Error s
 
-let fold_l f acc l = fold_seq f acc (fun k -> List.iter k l)
+let foldList f acc l = foldSeq f acc (fun k -> List.iter k l)
 
 (** {2 Misc} *)
 
@@ -199,75 +194,16 @@ let retry n f =
       | Error e -> retry (n-1) (e::acc)
   in retry n []
 
-(** {2 Infix} *)
-
-module Infix = struct
-  let (>>=) = (>>=)
-  let (>|=) = (>|=)
-  let (<*>) = (<*>)
-end
-
-(** {2 Monadic Operations} *)
-
-module type MONAD = sig
-  type 'a t
-  val return : 'a -> 'a t
-  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
-end
-
-module Traverse(M : MONAD) = struct
-  let (>>=) = M.(>>=)
-
-  let map_m f e = match e with
-    | Error s -> M.return (Error s)
-    | Ok x -> f x >>= fun y -> M.return (Ok y)
-
-  let sequence_m m = map_m (fun x->x) m
-
-  let fold_m f acc e = match e with
-    | Error _ -> M.return acc
-    | Ok x -> f acc x >>= fun y -> M.return y
-
-  let retry_m n f =
-    let rec retry n acc = match n with
-      | 0 -> M.return (fail (List.rev acc))
-      | _ ->
-        f () >>= function
-        | Ok x -> M.return (Ok x)
-        | Error e -> retry (n-1) (e::acc)
-    in retry n []
-end
-
 (** {2 Conversions} *)
 
-let to_opt = function
+let toOption = function
   | Ok x -> Some x
   | Error _ -> None
 
-let of_opt = function
+let fromOption = function
   | None -> Error "of_opt"
   | Some x -> Ok x
 
-let to_seq e k = match e with
+let toSeq e k = match e with
   | Ok x -> k x
   | Error _ -> ()
-
-type ('a, 'b) error = [`Ok of 'a | `Error of 'b]
-
-let of_err = function
-  | `Ok x -> Ok x
-  | `Error y -> Error y
-
-let to_err = function
-  | Ok x -> `Ok x
-  | Error y -> `Error y
-
-(** {2 IO} *)
-
-let pp pp_x fmt e = match e with
-  | Ok x -> Format.fprintf fmt "@[ok(@,%a)@]" pp_x x
-  | Error s -> Format.fprintf fmt "@[error(@,%s)@]" s
-
-let pp' pp_x pp_e fmt e = match e with
-  | Ok x -> Format.fprintf fmt "@[ok(@,%a)@]" pp_x x
-  | Error s -> Format.fprintf fmt "@[error(@,%a)@]" pp_e s
--- src/bresult.mli
+++ src/bresult.mli
@@ -18,81 +18,70 @@ type (+'good, +'bad) t = ('good, 'bad) Result.result =
   | Ok of 'good
   | Error of 'bad
 
-val return : 'a -> ('a, 'err) t
+(* was return *)
+val make : 'a -> ('a, 'err) t
 (** Successfully return a value *)
 
 val fail : 'err -> ('a, 'err) t
 (** Fail with an error *)
 
-val of_exn : exn -> ('a, string) t
+val fromException : exn -> ('a, string) t
 (** [of_exn e] uses {!Printexc} to print the exception as a string *)
 
-val of_exn_trace : exn -> ('a, string) t
+val fromExceptionTrace : exn -> ('a, string) t
 (** [of_exn_trace e] is similar to [of_exn e], but it adds the stacktrace
     to the error message.
 
     Remember to call [Printexc.record_backtrace true] and compile with the
     debug flag for this to work. *)
 
-val fail_printf : ('a, Buffer.t, unit, ('a, string) t) format4 -> 'a
-(** [fail_printf format] uses [format] to obtain an error message
-    and then returns [Error msg] *)
-
-val fail_fprintf : ('a, Format.formatter, unit, ('a, string) t) format4 -> 'a
-(** [fail_printf format] uses [format] to obtain an error message
-    and then returns [Error msg] *)
-
 val map : ('a -> 'b) -> ('a, 'err) t -> ('b, 'err) t
 (** Map on success *)
 
-val map_err : ('err1 -> 'err2) -> ('a, 'err1) t -> ('a, 'err2) t
+val mapError : ('err1 -> 'err2) -> ('a, 'err1) t -> ('a, 'err2) t
 (** Map on the error variant *)
 
 val map2 : ('a -> 'b) -> ('err1 -> 'err2) -> ('a, 'err1) t -> ('b, 'err2) t
 (** Same as {!map}, but also with a function that can transform
     the error message in case of failure *)
 
-val iter : ('a -> unit) -> ('a, _) t -> unit
+val forEach : ('a -> unit) -> ('a, _) t -> unit
 (** Apply the function only in case of Ok *)
 
-exception Get_error
+exception GetError
 
-val get_exn : ('a, _) t -> 'a
+val getOrRaise : ('a, _) t -> 'a
 (** Extract the value [x] from [Ok x], fails otherwise.
     You should be careful with this function, and favor other combinators
     whenever possible.
     @raise Get_error if the value is an error. *)
 
-val get_or : ('a, _) t -> default:'a -> 'a
+val getOr : ('a, _) t -> default:'a -> 'a
 (** [get_or e ~default] returns [x] if [e = Ok x], [default] otherwise *)
 
-val map_or : ('a -> 'b) ->  ('a, 'c) t -> default:'b -> 'b
+val mapOr : ('a -> 'b) ->  ('a, 'c) t -> default:'b -> 'b
 (** [map_or f e ~default] returns [f x] if [e = Ok x], [default] otherwise *)
 
 val catch : ('a, 'err) t -> ok:('a -> 'b) -> err:('err -> 'b) -> 'b
 (** [catch e ~ok ~err] calls either [ok] or [err] depending on
     the value of [e]. *)
 
-val flat_map : ('a -> ('b, 'err) t) -> ('a, 'err) t -> ('b, 'err) t
-
-val (>|=) : ('a, 'err) t -> ('a -> 'b) -> ('b, 'err) t
-
-val (>>=) : ('a, 'err) t -> ('a -> ('b, 'err) t) -> ('b, 'err) t
+val flatMap : ('a -> ('b, 'err) t) -> ('a, 'err) t -> ('b, 'err) t
 
 val equal : ?err:'err equal -> 'a equal -> ('a, 'err) t equal
 
 val compare : ?err:'err ord -> 'a ord -> ('a, 'err) t ord
 
-val fold : ok:('a -> 'b) -> error:('err -> 'b) -> ('a, 'err) t -> 'b
+val reduce : ok:('a -> 'b) -> error:('err -> 'b) -> ('a, 'err) t -> 'b
 (** [fold ~ok ~error e] opens [e] and, if [e = Ok x], returns
     [ok x], otherwise [e = Error s] and it returns [error s]. *)
 
-val is_ok : ('a, 'err) t -> bool
+val isOk : ('a, 'err) t -> bool
 (** Return true if Ok/
 
     @since 1.0 *)
 
-val is_error : ('a, 'err) t -> bool
+val isError : ('a, 'err) t -> bool
 (** Return true if Error
 
     @since 1.0 *)
@@ -103,10 +92,10 @@ val guard : (unit -> 'a) -> ('a, exn) t
 (** [guard f] runs [f ()] and returns its result wrapped in [Ok]. If
     [f ()] raises some exception [e], then it fails with [Error e] *)
 
-val guard_str : (unit -> 'a) -> ('a, string) t
+val guardToString : (unit -> 'a) -> ('a, string) t
 (** Same as {!guard} but uses {!of_exn} to print the exception. *)
 
-val guard_str_trace : (unit -> 'a) -> ('a, string) t
+val guardToStringTrace : (unit -> 'a) -> ('a, string) t
 (** Same as {!guard_str} but uses {!of_exn_trace} instead of {!of_exn} so
     that the stack trace is printed. *)
 
@@ -121,10 +110,8 @@ val wrap3 : ('a -> 'b -> 'c -> 'd) -> 'a -> 'b -> 'c -> ('d, exn) t
 
 (** {2 Applicative} *)
 
-val pure : 'a -> ('a, 'err) t
-(** Synonym of {!return} *)
-
-val (<*>) : ('a -> 'b, 'err) t -> ('a, 'err) t -> ('b, 'err) t
+(* was <*> *)
+val apply : ('a -> 'b, 'err) t -> ('a, 'err) t -> ('b, 'err) t
 (** [a <*> b] evaluates [a] and [b], and, in case of success, returns
     [Ok (a b)]. Otherwise, it fails, and the error of [a] is chosen
     over the error of [b] if both fail. *)
@@ -140,19 +127,13 @@ val both : ('a, 'err) t  -> ('b, 'err) t -> (('a * 'b), 'err) t
 
 (** {2 Infix} *)
 
-module Infix : sig
-  val (>|=) : ('a, 'err) t -> ('a -> 'b) -> ('b, 'err) t
-  val (>>=) : ('a, 'err) t -> ('a -> ('b, 'err) t) -> ('b, 'err) t
-  val (<*>) : ('a -> 'b, 'err) t -> ('a, 'err) t -> ('b, 'err) t
-end
-
 (** {2 Collections} *)
 
-val map_l : ('a -> ('b, 'err) t) -> 'a list -> ('b list, 'err) t
+val mapList : ('a -> ('b, 'err) t) -> 'a list -> ('b list, 'err) t
 
-val fold_l : ('b -> 'a -> ('b, 'err) t) -> 'b -> 'a list -> ('b, 'err) t
+val foldList : ('b -> 'a -> ('b, 'err) t) -> 'b -> 'a list -> ('b, 'err) t
 
-val fold_seq : ('b -> 'a -> ('b, 'err) t) -> 'b -> 'a sequence -> ('b, 'err) t
+val foldSeq : ('b -> 'a -> ('b, 'err) t) -> 'b -> 'a sequence -> ('b, 'err) t
 
 (** {2 Misc} *)
 
@@ -165,42 +146,10 @@ val retry : int -> (unit -> ('a, 'err) t) -> ('a, 'err list) t
     of [f ()] that doesn't fail. If [f] fails [n] times, [retry n f] fails
     with the list of successive errors. *)
 
-(** {2 Monadic Operations} *)
-module type MONAD = sig
-  type 'a t
-  val return : 'a -> 'a t
-  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
-end
-
-module Traverse(M : MONAD) : sig
-  val sequence_m : ('a M.t, 'err) t -> ('a, 'err) t M.t
-
-  val fold_m : ('b -> 'a -> 'b M.t) -> 'b -> ('a, 'err) t -> 'b M.t
-
-  val map_m : ('a -> 'b M.t) -> ('a, 'err) t -> ('b, 'err) t M.t
-
-  val retry_m : int -> (unit -> ('a, 'err) t M.t) -> ('a, 'err list) t M.t
-end
-
 (** {2 Conversions} *)
 
-val to_opt : ('a, _) t -> 'a option
+val toOption : ('a, _) t -> 'a option
 
-val of_opt : 'a option -> ('a, string) t
+val fromOption : 'a option -> ('a, string) t
 
-val to_seq : ('a, _) t -> 'a sequence
-
-type ('a, 'b) error = [`Ok of 'a | `Error of 'b]
-
-val of_err : ('a, 'b) error -> ('a, 'b) t
-(** @since 0.17 *)
-
-val to_err : ('a, 'b) t -> ('a, 'b) error
-(** @since 0.17 *)
-
-(** {2 IO} *)
-
-val pp : 'a printer -> ('a, string) t printer
-
-val pp': 'a printer -> 'e printer -> ('a, 'e) t printer
-(** Printer that is generic on the error type *)
+val toSeq : ('a, _) t -> 'a sequence
