8d8a57996b0f1eb65069752607aa18bd7e3ad046
Bob Fang <dorafmon@gmail.com>
2017-03-29 08:50:46 +0100

    Fix the test function names

--- test/test.ml
+++ test/test.ml
@@ -1,12 +1,12 @@
 type t
 
 external ava:< 
-test : string -> (t -> unit) -> unit[@bs.meth]
+    test : string -> (t -> unit) -> unit[@bs.meth]
 > Js.t = "ava" [@@bs.module]
 
-let test = fun run -> ava##test "" run
+let test = fun name run -> ava##test name run
 
-let testWithName = fun name run -> ava##test name run
+let test_ = fun run -> ava##test "" run
 
 external deepEqual: t -> 'a -> 'a -> unit = "deepEqual" [@@bs.send]
 
@@ -14,7 +14,7 @@ external pass: t -> unit -> unit = "" [@@bs.send]
 
 external fail: t -> unit -> unit = "" [@@bs.send]
 
-let () = testWithName "dummy" (fun t ->
+let () = test "dummy" (fun t ->
     deepEqual t 1 1;
     pass t ()
 )
--- test/test.mli
+++ test/test.mli
@@ -1,9 +1,9 @@
 (*Ideally we should have a unified interface of testing framework, I am still thinking about how to abstract away the difference between JS testing and OUnit.*)
 type t
 
-val test: (t -> unit) -> unit
+val test: string -> (t -> unit) -> unit
 
-val testWithName: string -> (t -> unit) -> unit
+val test_: (t -> unit) -> unit
 
 val deepEqual: t -> 'a -> 'a -> unit
 
