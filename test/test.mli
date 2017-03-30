4b188b1152922535fb130fedfc443d603678646e
glennsl <glenn@slotte.net>
2017-03-30 05:08:23 +0200

    Improved test setup

--- test/test.mli
+++ test/test.mli
@@ -4,6 +4,8 @@ type t
 val test: string -> (t -> unit) -> unit
 val test_: (t -> unit) -> unit
 
-val pass: t -> unit -> unit
-val fail: t -> unit -> unit
-val deepEqual: t -> 'a -> 'a -> unit
+external pass: t -> unit = "" [@@bs.send]
+external fail: t -> unit = "" [@@bs.send]
+external deepEqual: t -> 'a -> 'a -> unit = "deepEqual" [@@bs.send]
+
+external plan: t -> int -> unit = "" [@@bs.send]
